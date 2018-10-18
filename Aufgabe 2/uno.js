bbe643104f3632e3dfd68c8c06c77076260891d0
Cryray <43845424+Cryray@users.noreply.github.com>
2018-10-18 15:37:13 +0200

    Erster Versuch Deck Erstellung

--- Aufgabe 2/uno.js
+++ Aufgabe 2/uno.js
@@ -1,1 +1,16 @@
+var Aussetzen;
+var Richtungswechsel;
+var zweiziehen;
+var deck = [];
+var green = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+var greenspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
+var yellow = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+var yellowspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
+var red = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+var redspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
+var blue = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+var bluespecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
+var colorcards = [green, yellow, blue, red];
+var specialcards = [greenspecial, yellowspecial, redspecial, bluespecial];
+var blackcards = [];
 //# sourceMappingURL=uno.js.map
---
+++ Aufgabe 2/uno.ts
@@ -0,0 +1,24 @@
+let Aussetzen: string;
+let Richtungswechsel: string;
+let zweiziehen: string;
+
+let deck: string[] = [];
+
+
+
+let green: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
+let greenspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
+
+let yellow: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
+let yellowspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
+
+let red: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
+let redspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
+
+let blue: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
+let bluespecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
+
+let colorcards: any[]=[green,yellow,blue,red];
+let specialcards: any[]=[greenspecial,yellowspecial,redspecial,bluespecial];
+
+let blackcards: string[]=[];
