8a0668c6354fb4ad892e6375811fc88cff319453
Cryray <43845424+Cryray@users.noreply.github.com>
2018-10-18 15:40:53 +0200

    Deck erstellt

--- Aufgabe 2/uno.js
+++ Aufgabe 2/uno.js
@@ -1,7 +1,8 @@
 var Aussetzen;
 var Richtungswechsel;
 var zweiziehen;
-var deck = [];
+var plusvier;
+var farbwechsel;
 var green = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 var greenspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
 var yellow = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
@@ -10,7 +11,8 @@ var red = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 var redspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
 var blue = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 var bluespecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
+var blackcards = [farbwechsel, farbwechsel, farbwechsel, farbwechsel, plusvier, plusvier, plusvier, plusvier];
 var colorcards = [green, yellow, blue, red];
 var specialcards = [greenspecial, yellowspecial, redspecial, bluespecial];
-var blackcards = [];
+var deck = [blackcards, colorcards, specialcards];
 //# sourceMappingURL=uno.js.map
--- Aufgabe 2/uno.ts
+++ Aufgabe 2/uno.ts
@@ -1,8 +1,10 @@
 let Aussetzen: string;
 let Richtungswechsel: string;
 let zweiziehen: string;
+let plusvier: string;
+let farbwechsel: string;
 
-let deck: string[] = [];
+
 
 
 
@@ -18,7 +20,9 @@ let redspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel
 let blue: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
 let bluespecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
 
+let blackcards: string[]=[farbwechsel,farbwechsel,farbwechsel,farbwechsel,plusvier,plusvier,plusvier,plusvier]
 let colorcards: any[]=[green,yellow,blue,red];
 let specialcards: any[]=[greenspecial,yellowspecial,redspecial,bluespecial];
 
-let blackcards: string[]=[];
+
+let deck: any[] = [blackcards,colorcards,specialcards];
