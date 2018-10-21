fac8cc98cca8415cf38b62ec74e6cabed13b71e3
Cryray <43845424+Cryray@users.noreply.github.com>
2018-10-21 16:02:47 +0200

    Letzte Änderung ( Hilfe durch Code von Nico Herrmann)

--- Aufgabe 2/uno.js
+++ Aufgabe 2/uno.js
@@ -1,156 +1,236 @@
-var Aussetzen;
-var Richtungswechsel;
-var zweiziehen;
-var plusvier;
-var farbwechsel;
-var green = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
-var greenspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
-var yellow = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
-var yellowspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
-var red = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
-var redspecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
-var blue = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
-var bluespecial = [Aussetzen, Aussetzen, Richtungswechsel, Richtungswechsel, zweiziehen];
-var blackcards1 = [plusvier, plusvier, plusvier, plusvier];
-var blackcards2 = [farbwechsel, farbwechsel, farbwechsel, farbwechsel];
-var colorcards = [green, yellow, blue, red];
-var specialcards = [greenspecial, yellowspecial, redspecial, bluespecial];
-var deck = [blackcards1, blackcards2, colorcards, specialcards];
-var c = "";
-function random(_n) {
-    return Math.floor(Math.random() * Math.floor(_n));
-}
-function placeDiv(_color, _n, _x) {
-    var div = document.createElement("div");
-    document.body.appendChild(div);
-    div.setAttribute("id", "a" + _x);
-    var s = div.style;
-    s.border = "thin solid black";
-    s.textAlign = "center";
-    s.position = "absolute";
-    s.backgroundColor = _color;
-    s.width = 50 + "px";
-    s.height = 130 + "px";
-    s.left = (_x + 0.5) * 100 + "px";
-    s.bottom = 40 + "px";
-    s.borderRadius = 5 + "px";
-    if (_color == "#000000") {
-        s.color = "white";
+var uno;
+(function (uno) {
+    var c0 = {
+        name: "0",
+        rot: 1,
+        gelb: 1,
+        gruen: 1,
+        blau: 1,
+    };
+    var c1 = {
+        name: "1",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c2 = {
+        name: "2",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c3 = {
+        name: "3",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c4 = {
+        name: "4",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c5 = {
+        name: "5",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c6 = {
+        name: "6",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c7 = {
+        name: "7",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c8 = {
+        name: "8",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c9 = {
+        name: "9",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c10 = {
+        name: "R",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c11 = {
+        name: "+2",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var c12 = {
+        name: "A",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    };
+    var s1 = 4;
+    var s2 = 4;
+    var c = "";
+    var AlleKarten = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12];
+    function random(_n) {
+        return Math.floor(Math.random() * Math.floor(_n));
     }
-    if (_color == "#0000ff") {
-        s.color = "white";
-    }
-}
-function main() {
-    var z;
-    var i = prompt("Wie viele Karten pro Spieler?");
-    z = Number(i);
-    for (var d = 0; d < z; d++) {
-        var l = random(15);
-        if (l == 13 && plusvier > 0) {
-            c = "#000000";
-            plusvier--;
-            placeDiv(c, plusvier, d);
-            continue;
-        }
-        else if (l == 13 && plusvier <= 0) {
-            d--;
-            continue;
-        }
-        else {
-            if (l == 14 && farbwechsel > 0) {
-                c = "#000000";
-                farbwechsel--;
-                placeDiv(c, farbwechsel, d);
-                continue;
-            }
-            else if (l == 14 && farbwechsel <= 0) {
-                d--;
-                continue;
-            }
-            else {
-                var r = random(4);
-                switch (r) {
-                    case 0:
-                        c = "#ff0000"; //rot
-                        if (deck[l].red > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].red--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                    case 1:
-                        c = "#00ff00";
-                        if (deck[l].green > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].green--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                    case 2:
-                        c = "#0000ff";
-                        if (deck[l].blue > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].blue--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                    case 3:
-                        c = "#ffff00";
-                        if (deck[l].yellow > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].yellow--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                }
-            }
-        }
-    }
-    function Stapel(_n) {
+    function placeDiv(_color, _n, _x) {
         var div = document.createElement("div");
         document.body.appendChild(div);
-        var s = div.style;
-        s.border = "thin solid black";
-        s.position = "absolute";
-        s.backgroundColor = "#f0f0f0";
-        s.width = 50 + "px";
-        s.height = 130 + "px";
-        s.left = (_n + 0.5) * 20 + "px";
-        s.top = (_n + 0.5) * 10 + "px";
-        s.borderRadius = 5 + "px";
-    }
-    function Ablage() {
-        var div = document.createElement("div");
-        document.body.appendChild(div);
-        div.setAttribute("id", "Ablage");
-        document.getElementById("Ablage").innerHTML += "Ablage";
+        div.setAttribute("id", "a" + _x);
+        document.getElementById("a" + _x).innerHTML += _n;
         var s = div.style;
         s.border = "thin solid black";
         s.textAlign = "center";
         s.position = "absolute";
-        s.backgroundColor = "white";
-        s.width = 70 + "px";
-        s.height = 150 + "px";
-        s.right = 50 + "px";
-        s.top = 20 + "px";
+        s.backgroundColor = _color;
+        s.width = 80 + "px";
+        s.height = 130 + "px";
+        s.left = (_x + 0.5) * 100 + "px";
+        s.bottom = 40 + "px";
+        s.borderRadius = 6 + "px";
+        if (_color == "#000000") {
+            s.color = "white";
+        }
+        if (_color == "#0000ff") {
+            s.color = "white";
+        }
     }
-    for (var i_1 = 0; i_1 < 3; i_1++) {
-        Stapel(i_1);
+    function main() {
+        var z;
+        var i = prompt("Wie viele Karten pro Spieler?");
+        z = Number(i);
+        for (var d = 0; d < z; d++) {
+            var l = random(15); //l = Zufallswert von 0-14 (alle Karten)
+            if (l == 13 && s1 > 0) {
+                c = "#000000";
+                s1--;
+                placeDiv(c, "+4", d);
+                continue;
+            }
+            else if (l == 13 && s1 <= 0) {
+                d--;
+                continue;
+            }
+            else {
+                if (l == 14 && s2 > 0) {
+                    c = "#000000";
+                    s2--;
+                    placeDiv(c, "F", d);
+                    continue;
+                }
+                else if (l == 14 && s2 <= 0) {
+                    d--;
+                    continue;
+                }
+                else {
+                    var r = random(4); //random Zahl von 0-3 jeweils eine Farbe
+                    switch (r) {
+                        case 0:
+                            c = "#ff0000"; //rot
+                            if (AlleKarten[l].rot > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].rot--;
+                                continue;
+                            }
+                            else {
+                                d--;
+                                continue;
+                            }
+                        case 1:
+                            c = "#00ff00"; //grün
+                            if (AlleKarten[l].gruen > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].gruen--;
+                                continue;
+                            }
+                            else {
+                                d--;
+                                continue;
+                            }
+                        case 2:
+                            c = "#0000ff"; //blau
+                            if (AlleKarten[l].blau > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].blau--;
+                                continue;
+                            }
+                            else {
+                                d--;
+                                continue;
+                            }
+                        case 3:
+                            c = "#ffff00"; //gelb
+                            if (AlleKarten[l].gelb > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].gelb--;
+                                continue;
+                            }
+                            else {
+                                d--;
+                                continue;
+                            }
+                    }
+                }
+            }
+        }
+        function Stapel(_n) {
+            var div = document.createElement("div");
+            document.body.appendChild(div);
+            var s = div.style;
+            s.border = "thin solid black";
+            s.position = "absolute";
+            s.backgroundColor = "#f0f0f0";
+            s.width = 50 + "px";
+            s.height = 130 + "px";
+            s.left = (_n + 0.5) * 20 + "px";
+            s.top = (_n + 0.5) * 10 + "px";
+            s.borderRadius = 5 + "px";
+        }
+        function Ablage() {
+            var div = document.createElement("div");
+            document.body.appendChild(div);
+            div.setAttribute("id", "Ablage");
+            document.getElementById("Ablage").innerHTML += "Ablage";
+            var s = div.style;
+            s.border = "thin solid black";
+            s.textAlign = "center";
+            s.position = "absolute";
+            s.backgroundColor = "white";
+            s.width = 70 + "px";
+            s.height = 150 + "px";
+            s.right = 50 + "px";
+            s.top = 20 + "px";
+        }
+        for (var i_1 = 0; i_1 < 3; i_1++) {
+            Stapel(i_1);
+        }
+        Ablage();
     }
-    Ablage();
-}
-document.addEventListener('DOMContentLoaded', main);
+    document.addEventListener('DOMContentLoaded', main);
+})(uno || (uno = {}));
 //# sourceMappingURL=uno.js.map
--- Aufgabe 2/uno.ts
+++ Aufgabe 2/uno.ts
@@ -1,168 +1,281 @@
-let Aussetzen: string;
-let Richtungswechsel: string;
-let zweiziehen: string;
-let plusvier: number;
-let farbwechsel: number;
-
-
-
-
-
-let green: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
-let greenspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
-
-let yellow: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
-let yellowspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
-
-let red: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
-let redspecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
-
-let blue: number[]= [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
-let bluespecial: string[]= [Aussetzen,Aussetzen,Richtungswechsel,Richtungswechsel,zweiziehen];
-
-let blackcards1: number[]=[plusvier,plusvier,plusvier,plusvier]
-let blackcards2: number[]=[farbwechsel,farbwechsel,farbwechsel,farbwechsel]
-let colorcards: any[]=[green,yellow,blue,red];
-let specialcards: any[]=[greenspecial,yellowspecial,redspecial,bluespecial];
-
-
-let deck: any[] = [blackcards1,blackcards2,colorcards,specialcards];
-let c: string="";
-function random(_n:number) {
-    return Math.floor(Math.random() * Math.floor(_n));
-}
-function placeDiv(_color:string, _n:number, _x:number) {
-    let div = document.createElement("div");
-    document.body.appendChild(div);
-    div.setAttribute("id", "a" + _x); 
-    let s = div.style;
-    s.border = "thin solid black";
-    s.textAlign = "center";
-    s.position = "absolute";
-    s.backgroundColor = _color;
-    s.width = 50 + "px";
-    s.height = 130 + "px";
-    s.left = (_x + 0.5) * 100 + "px";
-    s.bottom = 40 + "px";
-    s.borderRadius = 5 + "px";
-    if (_color == "#000000") {
-        s.color = "white";
+namespace uno { 
+    interface Cards {
+        name: string;
+        rot: number;
+        gelb: number;
+        gruen: number;
+        blau: number;
     }
-    if (_color == "#0000ff") {
-        s.color = "white";
+    let c0: Cards = {
+        name: "0",
+        rot: 1,
+        gelb: 1,
+        gruen: 1,
+        blau: 1,
     }
-}
 
-function main() {
-    let z:number;
-    let i = prompt("Wie viele Karten pro Spieler?");
-    z = Number(i);
-    for (let d = 0; d < z; d++) {
-        let l = random(15); 
-        if (l == 13 && plusvier > 0) {
-            c = "#000000";
-            plusvier--;
-            placeDiv(c, plusvier, d);
-            continue;
+    let c1: Cards = {
+        name: "1",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c2: Cards = {
+        name: "2",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c3: Cards = {
+        name: "3",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c4: Cards = {
+        name: "4",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c5: Cards = {
+        name: "5",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c6: Cards = {
+        name: "6",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c7: Cards = {
+        name: "7",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c8: Cards = {
+        name: "8",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c9: Cards = {
+        name: "9",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c10: Cards = {
+        name: "R",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c11: Cards = {
+        name: "+2",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let c12: Cards = {
+        name: "A",
+        rot: 2,
+        gelb: 2,
+        gruen: 2,
+        blau: 2,
+    }
+
+    let s1: number = 4; 
+    let s2: number = 4; 
+
+    let c: string = ""; 
+
+    let AlleKarten = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12] 
+    
+    function random(_n: number): number 
+    { 
+        return Math.floor(Math.random() * Math.floor(_n));
+    }
+
+
+
+    function placeDiv(_color: string, _n: string, _x: number): void {
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "a" + _x) 
+
+        document.getElementById("a" + _x).innerHTML += _n; 
+       
+        let s: CSSStyleDeclaration = div.style;
+        s.border = "thin solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = _color;
+        s.width = 80 + "px";
+        s.height = 130 + "px";
+        s.left = (_x + 0.5) * 100 + "px";
+        s.bottom = 40 + "px";
+        s.borderRadius = 6 + "px";
+        
+
+
+        if (_color == "#000000") { //schwarz
+            s.color = "white";
         }
-        else if (l == 13 && plusvier <= 0) {
-            d--;
-            continue;
+
+        if (_color == "#0000ff") { //Blau
+            s.color = "white";
         }
-        else {
-            if (l == 14 && farbwechsel > 0) {
+    }
+
+
+    
+    function main(): void {
+        let z: number;
+        let i: string = prompt("Wie viele Karten pro Spieler?");
+        z = Number(i);
+        for (let d: number = 0; d < z; d++) { //d = aktuelle Karte die gegeben wird
+            let l: number = random(15); //l = Zufallswert von 0-14 (alle Karten)
+            if (l == 13 && s1 > 0) { //Wenn l = 13 & s1 > 0 dann generiere schwarze +4
                 c = "#000000";
-                farbwechsel--;
-                placeDiv(c, farbwechsel, d);
+                s1--;
+                placeDiv(c, "+4", d);
                 continue;
+
             }
-            else if (l == 14 && farbwechsel <= 0) {
+            else if (l == 13 && s1 <= 0) { //Wenn l = 13 & s1 <= 0 dann continue (d--) da sonst Karte übersprungen
                 d--;
                 continue;
             }
             else {
-                let r = random(4);
-                switch (r) {
-                    case 0:
-                        c = "#ff0000"; //rot
-                        if (deck[l].red > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].red--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                    case 1:
-                        c = "#00ff00"; 
-                        if (deck[l].green > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].green--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                    case 2:
-                        c = "#0000ff"; 
-                        if (deck[l].blue > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].blue--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
-                    case 3:
-                        c = "#ffff00"; 
-                        if (deck[l].yellow > 0) {
-                            placeDiv(c, deck[l].name, d);
-                            deck[l].yellow--;
-                            continue;
-                        }
-                        else {
-                            d--;
-                            continue;
-                        }
+                if (l == 14 && s2 > 0) { //Wenn l = 14 & s2 > 0 dann generiere schwarze Farbwahl
+                    c = "#000000";
+                    s2--;
+                    placeDiv(c, "F", d);
+                    continue;
                 }
-            }
-        }
-    }
-    function Stapel(_n:number) {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
-        let s = div.style;
-        s.border = "thin solid black";
-        s.position = "absolute";
-        s.backgroundColor = "#f0f0f0";
-        s.width = 50 + "px";
-        s.height = 130 + "px";
-        s.left = (_n + 0.5) * 20 + "px";
-        s.top = (_n + 0.5) * 10 + "px";
-        s.borderRadius = 5 + "px";
-    }
-    function Ablage() {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
-        div.setAttribute("id", "Ablage");
-        document.getElementById("Ablage").innerHTML += "Ablage";
-        let s = div.style;
-        s.border = "thin solid black";
-        s.textAlign = "center";
-        s.position = "absolute";
-        s.backgroundColor = "white";
-        s.width = 70 + "px";
-        s.height = 150 + "px";
-        s.right = 50 + "px";
-        s.top = 20 + "px";
-    }
-    for (let i = 0; i < 3; i++) {
-        Stapel(i);
-    }
-    Ablage();
-}
+                else if (l == 14 && s2 <= 0) { //Wenn l = 14 & s2 <= 0 dann continue (d-- da sonst Karte übersprungen)
+                    d--;
+                    continue;
+                }
+                else { //Wenn l =/= 13 || 14 dann vergebe Farbe an Kartenwert
+                    let r: number = random(4); //random Zahl von 0-3 jeweils eine Farbe
+                    switch (r) {
+                        case 0: //Wenn r = 0 dann rote Farbe
+                            c = "#ff0000"; //rot
+                            if (AlleKarten[l].rot > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].rot--;
+                                continue;
+                            }
+                            else { //Wenn Kombination aus Wert und Farbe <= 0 dann continue (d-- da sonst Karte übersprungen)
+                                d--;
+                                continue;
+                            }
+                        case 1: //Wenn r = 1 dann grüne Farbe
+                            c = "#00ff00"; //grün
+                            if (AlleKarten[l].gruen > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].gruen--;
+                                continue;
+                            }
+                            else { //Wenn Kombination aus Wert und Farbe <= 0 dann continue (d-- da sonst Karte übersprungen)
+                                d--;
+                                continue;
+                            }
+                        case 2: //Wenn r = 2 dann blaue Farbe
+                            c = "#0000ff"; //blau
+                            if (AlleKarten[l].blau > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].blau--;
+                                continue;
+                            }
+                            else { //Wenn Kombination aus Wert und Farbe <= 0 dann continue (d-- da sonst Karte übersprungen)
+                                d--;
+                                continue;
+                            }
+                        case 3: //Wenn r = 3 dann gelbe Farbe
+                            c = "#ffff00"; //gelb
+                            if (AlleKarten[l].gelb > 0) {
+                                placeDiv(c, AlleKarten[l].name, d);
+                                AlleKarten[l].gelb--;
+                                continue;
+                            }
+                            else { //Wenn Kombination aus Wert und Farbe <= 0 dann continue (d-- da sonst Karte übersprungen)
+                                d--;
+                                continue;
+                            }
+                    }
+                }
 
-document.addEventListener('DOMContentLoaded',main)
+
+            }
+
+        }
+        function Stapel(_n: number): void {
+            let div: HTMLDivElement = document.createElement("div");
+            document.body.appendChild(div);
+
+            let s: CSSStyleDeclaration = div.style;
+            s.border = "thin solid black";
+            s.position = "absolute";
+            s.backgroundColor = "#f0f0f0";
+            s.width = 50 + "px";
+            s.height = 130 + "px";
+            s.left = (_n + 0.5) * 20 + "px";
+            s.top = (_n + 0.5) * 10 + "px";
+            s.borderRadius = 5 + "px";
+        }
+
+        function Ablage(): void {
+            let div: HTMLDivElement = document.createElement("div");
+            document.body.appendChild(div);
+            div.setAttribute("id", "Ablage")
+
+            document.getElementById("Ablage").innerHTML += "Ablage";
+
+            let s: CSSStyleDeclaration = div.style;
+            s.border = "thin solid black";
+            s.textAlign = "center";
+            s.position = "absolute";
+            s.backgroundColor = "white";
+            s.width = 70 + "px";
+            s.height = 150 + "px";
+            s.right = 50 + "px";
+            s.top = 20 + "px";
+        }
+
+        for (let i: number = 0; i < 3; i++) {
+            Stapel(i);
+        }
+
+        Ablage();
+    }
+       
+    document.addEventListener('DOMContentLoaded',main);
+}
