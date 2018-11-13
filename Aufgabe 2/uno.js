3c8481f363dd69552575a85c698b3b5001a62cd7
Cryray <43845424+Cryray@users.noreply.github.com>
2018-11-13 09:29:58 +0100

    Änderungen mit Dell-Oro

--- Aufgabe 2/uno.js
+++ Aufgabe 2/uno.js
@@ -1,12 +1,8 @@
 var Uno;
 (function (Uno) {
     document.addEventListener("DOMContentLoaded", init);
-    function start() {
-        document.getElementById("span").addEventListener("click", playCard);
-        document.getElementById("Ziehstapel").addEventListener("click", drawCardbyClick);
-    }
     var hand = [];
-    var stapel = [];
+    var pile = [];
     var cards = [{ color: "#ff0000", value: "0" }, { color: "#ff0000", value: "1" }, { color: "#ff0000", value: "1" }, { color: "#ff0000", value: "2" }, { color: "#ff0000", value: "3" }, { color: "#ff0000", value: "3" }, { color: "#ff0000", value: "4" }, { color: "#ff0000", value: "4" }, { color: "#ff0000", value: "5" }, { color: "#ff0000", value: "5" }, { color: "#ff0000", value: "6" }, { color: "#ff0000", value: "6" }, { color: "#ff0000", value: "7" }, { color: "#ff0000", value: "7" }, { color: "#ff0000", value: "8" }, { color: "#ff0000", value: "8" }, { color: "#ff0000", value: "9" }, { color: "#ff0000", value: "9" }, { color: "#ff0000", value: "+2" }, { color: "#ff0000", value: "+2" }, { color: "#ff0000", value: "aussetzen" }, { color: "#ff0000", value: "aussetzen" },
         { color: "#00ff00", value: "0" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "aussetzen" }, { color: "#00ff00", value: "aussetzen" },
         { color: "#0000ff", value: "0" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "aussetzen" }, { color: "#0000ff", value: "aussetzen" },
@@ -16,53 +12,56 @@ var Uno;
         var numOfCards = prompt("Kartenanzahl eingeben");
         var numCards = +numOfCards;
         drawCards(numCards);
+        installEventListeners();
+    }
+    function installEventListeners() {
+        document.getElementById("hand").addEventListener("click", playCard);
+        document.getElementById("drawpile").addEventListener("click", drawOneCard);
     }
     function drawCards(_numCards) {
         for (var x = 0; x < _numCards; x++) {
             var random = Math.floor(Math.random() * cards.length);
-            var getcard = cards.splice(random, 1)[0];
-            hand.push(getcard);
+            var getCard = cards.splice(random, 1)[0];
+            hand.push(getCard);
             displayHand(hand);
         }
     }
     function displayHand(hand) {
-        var handdiv = document.getElementById("hand");
+        var handDiv = document.getElementById("hand");
         var span = document.createElement("span");
         for (var o = 0; o < hand.length; o++) {
             span.innerText = hand[o].value;
             span.style.backgroundColor = hand[o].color;
             span.style.color = "black";
+            span.id = o.toString();
             if (hand[o].color == "#000000" || hand[o].color == "#0000ff") {
                 span.style.color = "white";
             }
-            handdiv.appendChild(span);
+            handDiv.appendChild(span);
         }
     }
     function playCard(_event) {
-        var CardsonHand = document.getElementById("span");
+        var cardsOnHand = document.getElementById("span");
         var parCard = _event.target;
-        if (parCard != CardsonHand) {
+        if (parCard != cardsOnHand) {
             var index = void 0;
-            var domAttribute = parCard.getAttribute("id");
-            domAttribute = domAttribute.substr(1);
-            index = parseInt(domAttribute);
+            var cardId = parCard.getAttribute("id");
+            index = parseInt(cardId);
             var card = hand.splice(index, 1)[0];
-            stapel.push(card);
+            pile.push(card);
         }
     }
-    function drawCard(_cards, _parnum) {
+    function drawOneCard(_event) {
+        drawCard(1);
+    }
+    function drawCard(_cards) {
         if (cards.length > 0) {
             for (var i = 0; i < _cards; i++) {
-                var maxNumber = cards.length;
-                var random = Math.floor(Math.random() * Math.floor(_parnum));
-                ;
+                var random = Math.floor(Math.random() * cards.length);
                 var card = cards.splice(random, 1)[0];
                 hand.push(card);
             }
         }
-    }
-    function drawCardbyClick() {
-        start();
     }
 })(Uno || (Uno = {}));
 //# sourceMappingURL=uno.js.map
--- Aufgabe 2/uno.ts
+++ Aufgabe 2/uno.ts
@@ -5,76 +5,77 @@ namespace Uno {
         value: string;
     }
 
-    document.addEventListener("DOMContentLoaded", init);
-    function start():void {
-        document.getElementById("span").addEventListener("click", playCard);
-        document.getElementById("Ziehstapel").addEventListener("click" ,drawCardbyClick)
-        } 
+    document.addEventListener( "DOMContentLoaded", init );
+    
+    
     let hand: Card[] = [];
-    let stapel:Card[]=[];
+    let pile: Card[] = [];
     let cards: Card[] = [{ color: "#ff0000", value: "0" }, { color: "#ff0000", value: "1" }, { color: "#ff0000", value: "1" }, { color: "#ff0000", value: "2" }, { color: "#ff0000", value: "3" }, { color: "#ff0000", value: "3" }, { color: "#ff0000", value: "4" }, { color: "#ff0000", value: "4" }, { color: "#ff0000", value: "5" }, { color: "#ff0000", value: "5" }, { color: "#ff0000", value: "6" }, { color: "#ff0000", value: "6" }, { color: "#ff0000", value: "7" }, { color: "#ff0000", value: "7" }, { color: "#ff0000", value: "8" }, { color: "#ff0000", value: "8" }, { color: "#ff0000", value: "9" }, { color: "#ff0000", value: "9" }, { color: "#ff0000", value: "+2" }, { color: "#ff0000", value: "+2" }, { color: "#ff0000", value: "aussetzen" }, { color: "#ff0000", value: "aussetzen" },
-        { color: "#00ff00", value: "0" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "aussetzen" }, { color: "#00ff00", value: "aussetzen" },
-        { color: "#0000ff", value: "0" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "aussetzen" }, { color: "#0000ff", value: "aussetzen" },
-        { color: "#ffff00", value: "0" }, { color: "#ffff00", value: "1" }, { color: "#ffff00", value: "1" }, { color: "#ffff00", value: "2" }, { color: "#ffff00", value: "2" }, { color: "#ffff00", value: "3" }, { color: "#ffff00", value: "3" }, { color: "#ffff00", value: "4" }, { color: "#ffff00", value: "4" }, { color: "#ffff00", value: "5" }, { color: "#ffff00", value: "5" }, { color: "#ffff00", value: "6" }, { color: "#ffff00", value: "6" }, { color: "#ffff00", value: "7" }, { color: "#ffff00", value: "7" }, { color: "#ffff00", value: "8" }, { color: "#ffff00", value: "8" }, { color: "#ffff00", value: "9" }, { color: "#ffff00", value: "9" }, { color: "#ffff00", value: "+2" }, { color: "#ffff00", value: "+2" }, { color: "#ffff00", value: "aussetzen" }, { color: "#ffff00", value: "aussetzen" },
-        { color: "#000000", value: "+4" }, { color: "#000000", value: "+4" }, { color: "#000000", value: "+4" }, { color: "#000000", value: "+4" }, { color: "#000000", value: "farbwechsel" }, { color: "#000000", value: "farbwechsel" }, { color: "#000000", value: "farbwechsel" }, { color: "#000000", value: "farbwechsel" }];
+    { color: "#00ff00", value: "0" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "1" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "2" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "3" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "4" }, { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "5" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "6" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "7" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "8" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "9" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "+2" }, { color: "#00ff00", value: "aussetzen" }, { color: "#00ff00", value: "aussetzen" },
+    { color: "#0000ff", value: "0" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "1" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "2" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "3" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "4" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "5" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "6" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "7" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "8" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "9" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "+2" }, { color: "#0000ff", value: "aussetzen" }, { color: "#0000ff", value: "aussetzen" },
+    { color: "#ffff00", value: "0" }, { color: "#ffff00", value: "1" }, { color: "#ffff00", value: "1" }, { color: "#ffff00", value: "2" }, { color: "#ffff00", value: "2" }, { color: "#ffff00", value: "3" }, { color: "#ffff00", value: "3" }, { color: "#ffff00", value: "4" }, { color: "#ffff00", value: "4" }, { color: "#ffff00", value: "5" }, { color: "#ffff00", value: "5" }, { color: "#ffff00", value: "6" }, { color: "#ffff00", value: "6" }, { color: "#ffff00", value: "7" }, { color: "#ffff00", value: "7" }, { color: "#ffff00", value: "8" }, { color: "#ffff00", value: "8" }, { color: "#ffff00", value: "9" }, { color: "#ffff00", value: "9" }, { color: "#ffff00", value: "+2" }, { color: "#ffff00", value: "+2" }, { color: "#ffff00", value: "aussetzen" }, { color: "#ffff00", value: "aussetzen" },
+    { color: "#000000", value: "+4" }, { color: "#000000", value: "+4" }, { color: "#000000", value: "+4" }, { color: "#000000", value: "+4" }, { color: "#000000", value: "farbwechsel" }, { color: "#000000", value: "farbwechsel" }, { color: "#000000", value: "farbwechsel" }, { color: "#000000", value: "farbwechsel" }];
 
     function init(): void {
-        let numOfCards: string = prompt("Kartenanzahl eingeben");
+        let numOfCards: string = prompt( "Kartenanzahl eingeben" );
         let numCards: number = +numOfCards;
 
-        drawCards(numCards);
+        drawCards( numCards );
+        installEventListeners();
     }
-
-    function drawCards(_numCards: number): void {
-        for (let x: number = 0; x < _numCards; x++) {
-            let random: number = Math.floor(Math.random() * cards.length);
-            let getcard: Card = cards.splice(random, 1)[0];
-            hand.push(getcard);
-            displayHand(hand);
+    function installEventListeners(): void {
+        document.getElementById( "hand" ).addEventListener( "click", playCard );
+        document.getElementById( "drawpile" ).addEventListener( "click", drawOneCard )
+    }
+    
+    function drawCards( _numCards: number ): void {
+        for ( let x: number = 0; x < _numCards; x++ ) {
+            let random: number = Math.floor( Math.random() * cards.length );
+            let getCard: Card = cards.splice( random, 1 )[0];
+            hand.push( getCard );
+            displayHand( hand );
         }
     }
 
-    function displayHand(hand: Card[]): void {
-        let handdiv: HTMLElement = document.getElementById("hand");
-        let span: HTMLSpanElement = document.createElement("span");
-        for (let o: number = 0; o < hand.length; o++) {
+    function displayHand( hand: Card[] ): void {
+        let handDiv: HTMLElement = document.getElementById( "hand" );
+        let span: HTMLSpanElement = document.createElement( "span" );
+        for ( let o: number = 0; o < hand.length; o++ ) {
             span.innerText = hand[o].value;
             span.style.backgroundColor = hand[o].color;
-            span.style.color ="black";
-
-            if (hand[o].color == "#000000"||hand[o].color == "#0000ff") {
+            span.style.color = "black";
+            span.id = o.toString();
+            if ( hand[o].color == "#000000" || hand[o].color == "#0000ff" ) {
                 span.style.color = "white";
             }
 
-            handdiv.appendChild(span);
+            handDiv.appendChild( span );
         }
     }
-    function playCard(_event: Event): void {
-        let CardsonHand: HTMLElement = document.getElementById("span");
+    function playCard( _event: Event ): void {
+        let cardsOnHand: HTMLElement = document.getElementById( "span" );
         let parCard: HTMLElement = <HTMLElement>_event.target;
-        if (parCard != CardsonHand) {
+        if ( parCard != cardsOnHand ) {
             let index: number;
-            let domAttribute: string = parCard.getAttribute("id");
-            domAttribute = domAttribute.substr(1);
-            index = parseInt(domAttribute);
-            let card: Card = hand.splice(index, 1)[0];
-            stapel.push(card);
-        }  
-        }     
-function drawCard(_cards: number, _parnum: number): void {
-    if (cards.length > 0) {
-        for (let i: number = 0; i < _cards; i++) {
-            let maxNumber: number = cards.length;
-            let random: number = Math.floor(Math.random() * Math.floor(_parnum));;
-            let card: Card = cards.splice(random, 1)[0];
-            hand.push(card);
+            let cardId: string = parCard.getAttribute( "id" );
+            index = parseInt( cardId );
+            let card: Card = hand.splice( index, 1 )[0];
+            pile.push( card );
         }
     }
-}
-function drawCardbyClick(): void {
-    drawCard (1);
-    start();
-}
-
-
+    
+    function drawOneCard (_event: Event):void {
+        drawCard(1);
+    }
+    
+    function drawCard( _cards: number ): void {
+        if ( cards.length > 0 ) {
+            for ( let i: number = 0; i < _cards; i++ ) {
+                let random: number = Math.floor( Math.random()*cards.length );
+                let card: Card = cards.splice( random, 1 )[0];
+                hand.push( card );
+            }
+        }
+    }
+    
 }
