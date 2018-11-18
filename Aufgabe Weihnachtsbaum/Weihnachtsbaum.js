588a61eed37714a5b20c2315c42c605f839c0c36
Cryray <43845424+Cryray@users.noreply.github.com>
2018-11-18 15:33:20 +0100

    yeet

--- Aufgabe Weihnachtsbaum/Weihnachtsbaum.js
+++ Aufgabe Weihnachtsbaum/Weihnachtsbaum.js
@@ -1,17 +1,21 @@
 var Weihnachtsbaumkonfigurator;
 (function (Weihnachtsbaumkonfigurator) {
-    var ball = [{ name: "Ball", price: 3, color: "red", size: "small" }, { name: "Ball", price: 3, color: "red", size: "big" }, { name: "Ball", price: 3, color: "silver", size: "small" }, { name: "Ball", price: 3, color: "silver", size: "big" }, { name: "Ball", price: 3, color: "gold", size: "small" }, { name: "Ball", price: 3, color: "gold", size: "big" }, { name: "Ball", price: 3, color: "blue", size: "small" }];
+    var ball = [{ name: "Small red ball", price: 3, color: "red", size: "small" }, { name: "Big red ball", price: 3, color: "red", size: "big" }, { name: "Small silver ball", price: 3, color: "silver", size: "small" }, { name: "Big silver ball", price: 3, color: "silver", size: "big" }, { name: "Small golden ball", price: 3, color: "gold", size: "small" }, { name: "Big golden ball", price: 3, color: "gold", size: "big" }, { name: "Small blue ball", price: 3, color: "blue", size: "small" }];
     var tree = [{ name: "Small fir tree", price: 10, color: "green", size: "small" }, { name: "Medium fir tree", price: 15, color: "green", size: "medium" }, { name: "Big fir tree", price: 20, color: "green", size: "big" }, { name: "Small Caucasian fir", price: 12, color: "green", size: "small" }, { name: "Medium Caucasian fir", price: 17, color: "green", size: "medium" }, { name: "Big Caucasian fir", price: 12, color: "green", size: "big" }, { name: "Norway spruce", price: 14, color: "green", size: "medium" }, { name: "Bluespruce", price: 12, color: "green", size: "medium" }, { name: "Pine", price: 18, color: "green", size: "medium" }];
     var tinsel = [{ name: "Green tinsel", price: 3.50, color: "green", size: "normal" }, { name: "Blue tinsel", price: 3.50, color: "blue", size: "normal" }, { name: "Gold tinsel", price: 3.50, color: "gold", size: "normal" }, { name: "Silver tinsel", price: 3.50, color: "silver", size: "normal" }, { name: "Red tinsel", price: 3.50, color: "red", size: "normal" }];
-    var candle = [{ name: "candle", price: 1, color: "red", size: "small" }, { name: "candle", price: 1.50, color: "red", size: "medium" }, { name: "candle", price: 2, color: "red", size: "big" }, { name: "candle", price: 1, color: "white", size: "small" }, { name: "candle", price: 1.50, color: "white", size: "medium" }, { name: "candle", price: 2, color: "white", size: "big" }];
+    var candle = [{ name: "Small red candle", price: 1, color: "red", size: "small" }, { name: "Medium red candle", price: 1.50, color: "red", size: "medium" }, { name: "Big red candle", price: 2, color: "red", size: "big" }, { name: "small white candle", price: 1, color: "white", size: "small" }, { name: "medium white candle", price: 1.50, color: "white", size: "medium" }, { name: "Big white candle", price: 2, color: "white", size: "big" }];
     var shipment = [{ name: "DHL", price: 4.99, color: "", size: "" }, { name: "Hermes", price: 3.99, color: "", size: "" }, { name: "UPS", price: 6.99, color: "", size: "" }];
     document.addEventListener("DOMContentLoaded", init);
+    document.addEventListener("DOMContentLoaded", init2);
+    document.addEventListener("DOMContentLoaded", init3);
+    document.addEventListener("DOMContentLoaded", init4);
     function init() {
         var fieldsets = document.getElementsByTagName("fieldset");
         for (var i = 0; i < 1; i++) {
             var fieldset = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
             //document.getElementById("summary").addEventListener("click", summary);
+            //BAUMART
             var node_1 = document.getElementById("Baumart");
             var childNodeHtml_1 = void 0;
             var art_1 = tree;
@@ -27,22 +31,10 @@ var Weihnachtsbaumkonfigurator;
                 childNodeHtml_1 += "</option>";
             }
             childNodeHtml_1 += "</select>";
-            childNodeHtml_1 += "<select>";
-            childNodeHtml_1 += "<h3>Anzahl:</h3>";
-            childNodeHtml_1 += "<option value=clear selected></option>";
-            for (var i_2 = 0; i_2 < 6; i_2 += 1) {
-                childNodeHtml_1 += "<option value=lamettanum";
-                childNodeHtml_1 += i_2;
-                childNodeHtml_1 += " id=lamettanum";
-                childNodeHtml_1 += i_2;
-                childNodeHtml_1 += ">";
-                childNodeHtml_1 += i_2;
-                childNodeHtml_1 += "</option>";
-            }
-            childNodeHtml_1 += "</select>";
             childNodeHtml_1 += "</hr>";
             node_1.innerHTML += childNodeHtml_1;
         }
+        //LAMETTA    
         var node = document.getElementById("Lametta");
         var childNodeHtml;
         var art = tinsel;
@@ -73,6 +65,104 @@ var Weihnachtsbaumkonfigurator;
         childNodeHtml += "</select>";
         childNodeHtml += "</hr>";
         node.innerHTML += childNodeHtml;
+    }
+    //KERZEN
+    function init2() {
+        var fieldsets = document.getElementsByTagName("fieldset");
+        for (var i = 0; i < 1; i++) {
+            var fieldset = fieldsets[i];
+            var node = document.getElementById("Kerzen");
+            var childNodeHtml = void 0;
+            var art = candle;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Kerzen></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (var i_2 = 0; i_2 < candle.length; i_2++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i_2;
+                childNodeHtml += ">";
+                childNodeHtml += candle[i_2].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "<select>";
+            childNodeHtml += "<h3>Anzahl:</h3>";
+            childNodeHtml += "<option value=clear selected></option>";
+            for (var i_3 = 0; i_3 < 20; i_3 += 1) {
+                childNodeHtml += "<option value=candlenum";
+                childNodeHtml += i_3;
+                childNodeHtml += " id=candlenum";
+                childNodeHtml += i_3;
+                childNodeHtml += ">";
+                childNodeHtml += i_3;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
+    }
+    //KUGELN
+    function init3() {
+        var fieldsets = document.getElementsByTagName("fieldset");
+        for (var i = 0; i < 1; i++) {
+            var fieldset = fieldsets[i];
+            var node = document.getElementById("Kugeln");
+            var childNodeHtml = void 0;
+            var art = ball;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Kugeln></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (var i_4 = 0; i_4 < candle.length; i_4++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i_4;
+                childNodeHtml += ">";
+                childNodeHtml += ball[i_4].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "<select>";
+            childNodeHtml += "<h3>Anzahl:</h3>";
+            childNodeHtml += "<option value=clear selected></option>";
+            for (var i_5 = 0; i_5 < 20; i_5 += 1) {
+                childNodeHtml += "<option value=ballnum";
+                childNodeHtml += i_5;
+                childNodeHtml += " id=ballnum";
+                childNodeHtml += i_5;
+                childNodeHtml += ">";
+                childNodeHtml += i_5;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
+    }
+    //Versand
+    function init4() {
+        var fieldsets = document.getElementsByTagName("fieldset");
+        for (var i = 0; i < 1; i++) {
+            var fieldset = fieldsets[i];
+            var node = document.getElementById("Versand");
+            var childNodeHtml = void 0;
+            var art = shipment;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Versand></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (var i_6 = 0; i_6 < shipment.length; i_6++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i_6;
+                childNodeHtml += ">";
+                childNodeHtml += shipment[i_6].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
     }
     function handleChange(_event) {
     }
--- Aufgabe Weihnachtsbaum/Weihnachtsbaum.ts
+++ Aufgabe Weihnachtsbaum/Weihnachtsbaum.ts
@@ -6,22 +6,25 @@ namespace Weihnachtsbaumkonfigurator {
         size: string;
     }
 
-     let ball: Product[] = [{ name: "Ball", price: 3, color: "red", size: "small" }, { name: "Ball", price: 3, color: "red", size: "big" }, { name: "Ball", price: 3, color: "silver", size: "small" }, { name: "Ball", price: 3, color: "silver", size: "big" }, { name: "Ball", price: 3, color: "gold", size: "small" }, { name: "Ball", price: 3, color: "gold", size: "big" }, { name: "Ball", price: 3, color: "blue", size: "small" }];
+     let ball: Product[] = [{ name: "Small red ball", price: 3, color: "red", size: "small" }, { name: "Big red ball", price: 3, color: "red", size: "big" }, { name: "Small silver ball", price: 3, color: "silver", size: "small" }, { name: "Big silver ball", price: 3, color: "silver", size: "big" }, { name: "Small golden ball", price: 3, color: "gold", size: "small" }, { name: "Big golden ball", price: 3, color: "gold", size: "big" }, { name: "Small blue ball", price: 3, color: "blue", size: "small" }];
      let tree: Product[] = [{ name: "Small fir tree", price: 10, color: "green", size: "small" }, { name: "Medium fir tree", price: 15, color: "green", size: "medium" }, { name: "Big fir tree", price: 20, color: "green", size: "big" }, { name: "Small Caucasian fir", price: 12, color: "green", size: "small" }, { name: "Medium Caucasian fir", price: 17, color: "green", size: "medium" }, { name: "Big Caucasian fir", price: 12, color: "green", size: "big" }, { name: "Norway spruce", price: 14, color: "green", size: "medium" }, { name: "Bluespruce", price: 12, color: "green", size: "medium" }, { name: "Pine", price: 18, color: "green", size: "medium" }];
      let tinsel: Product[] = [{ name: "Green tinsel", price: 3.50, color: "green", size: "normal" }, { name: "Blue tinsel", price: 3.50, color: "blue", size: "normal" }, { name: "Gold tinsel", price: 3.50, color: "gold", size: "normal" }, { name: "Silver tinsel", price: 3.50, color: "silver", size: "normal" }, { name: "Red tinsel", price: 3.50, color: "red", size: "normal" }];
-     let candle: Product[] = [{ name: "candle", price: 1, color: "red", size: "small" }, { name: "candle", price: 1.50, color: "red", size: "medium" }, { name: "candle", price: 2, color: "red", size: "big" }, { name: "candle", price: 1, color: "white", size: "small" }, { name: "candle", price: 1.50, color: "white", size: "medium" }, { name: "candle", price: 2, color: "white", size: "big" }];
+     let candle: Product[] = [{ name: "Small red candle", price: 1, color: "red", size: "small" }, { name: "Medium red candle", price: 1.50, color: "red", size: "medium" }, { name: "Big red candle", price: 2, color: "red", size: "big" }, { name: "small white candle", price: 1, color: "white", size: "small" }, { name: "medium white candle", price: 1.50, color: "white", size: "medium" }, { name: "Big white candle", price: 2, color: "white", size: "big" }];
      let shipment: Product[] = [{ name: "DHL", price: 4.99, color: "", size: "" }, { name: "Hermes", price: 3.99, color: "", size: "" }, { name: "UPS", price: 6.99, color: "", size: "" }];
 
 
     document.addEventListener("DOMContentLoaded", init);
-
+    document.addEventListener("DOMContentLoaded", init2);
+    document.addEventListener("DOMContentLoaded", init3);
+    document.addEventListener("DOMContentLoaded", init4);
     function init(): void {
         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
         for (let i: number = 0; i < 1; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
             //document.getElementById("summary").addEventListener("click", summary);
-
+        
+            //BAUMART
             let node: HTMLElement = document.getElementById("Baumart");
             let childNodeHtml: string;
             let art: Product[] = tree;
@@ -39,24 +42,12 @@ namespace Weihnachtsbaumkonfigurator {
                 childNodeHtml += "</option>";
             }
             childNodeHtml += "</select>";
-            childNodeHtml += "<select>";
-            childNodeHtml += "<h3>Anzahl:</h3>";
-            childNodeHtml += "<option value=clear selected></option>";
-            for (let i: number = 0; i < 6; i += 1) {
-                childNodeHtml += "<option value=lamettanum";
-                childNodeHtml += i;
-                childNodeHtml += " id=lamettanum";
-                childNodeHtml += i;
-                childNodeHtml += ">";
-                childNodeHtml += i;
-                childNodeHtml += "</option>";
-            }
-            childNodeHtml += "</select>";
+         
             childNodeHtml += "</hr>";
 
             node.innerHTML += childNodeHtml;
-        }
-        
+    }
+//LAMETTA    
         let node: HTMLElement = document.getElementById("Lametta");
         let childNodeHtml: string;
         let art: Product[] = tinsel;
@@ -91,6 +82,54 @@ namespace Weihnachtsbaumkonfigurator {
 
         node.innerHTML += childNodeHtml;
     }
+//KERZEN
+    function init2(): void {
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+        for (let i: number = 0; i < 1; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+    
+    let node: HTMLElement = document.getElementById("Kerzen");
+    let childNodeHtml: string;
+    let art: Product[] = candle;
+
+    childNodeHtml = "";
+    childNodeHtml += "<select name=Select id=select>";
+    childNodeHtml += "<optgroup label=Kerzen></optgroup> ";
+    childNodeHtml += "<datalist id=options>";
+
+    for (let i: number = 0; i < candle.length; i++) {
+        childNodeHtml += "<option value=";
+        childNodeHtml += "lam" + i;
+        childNodeHtml += ">";
+        childNodeHtml += candle[i].name;
+        childNodeHtml += "</option>";
+    }
+    childNodeHtml += "</select>";
+    childNodeHtml += "<select>";
+    childNodeHtml += "<h3>Anzahl:</h3>";
+    childNodeHtml += "<option value=clear selected></option>";
+    for (let i: number = 0; i < 20; i += 1) {
+        childNodeHtml += "<option value=candlenum";
+        childNodeHtml += i;
+        childNodeHtml += " id=candlenum";
+        childNodeHtml += i;
+        childNodeHtml += ">";
+        childNodeHtml += i;
+        childNodeHtml += "</option>";
+    }
+    childNodeHtml += "</select>";
+    childNodeHtml += "</hr>";
+
+    node.innerHTML += childNodeHtml;
+        }
+    }
+    
+  //KUGELN
+    function init3(): void {
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+        for (let i: number = 0; i < 1; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+    
     let node: HTMLElement = document.getElementById("Kugeln");
     let childNodeHtml: string;
     let art: Product[] = ball;
@@ -100,7 +139,7 @@ namespace Weihnachtsbaumkonfigurator {
     childNodeHtml += "<optgroup label=Kugeln></optgroup> ";
     childNodeHtml += "<datalist id=options>";
 
-    for (let i: number = 0; i < ball.length; i++) {
+    for (let i: number = 0; i < candle.length; i++) {
         childNodeHtml += "<option value=";
         childNodeHtml += "lam" + i;
         childNodeHtml += ">";
@@ -124,7 +163,37 @@ namespace Weihnachtsbaumkonfigurator {
     childNodeHtml += "</hr>";
 
     node.innerHTML += childNodeHtml;
-}
+        }
+    }
+  //Versand
+    function init4(): void {
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+        for (let i: number = 0; i < 1; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+    
+    let node: HTMLElement = document.getElementById("Versand");
+    let childNodeHtml: string;
+    let art: Product[] = shipment;
+
+    childNodeHtml = "";
+    childNodeHtml += "<select name=Select id=select>";
+    childNodeHtml += "<optgroup label=Versand></optgroup> ";
+    childNodeHtml += "<datalist id=options>";
+
+    for (let i: number = 0; i < shipment.length; i++) {
+        childNodeHtml += "<option value=";
+        childNodeHtml += "lam" + i;
+        childNodeHtml += ">";
+        childNodeHtml += shipment[i].name;
+        childNodeHtml += "</option>";
+    }
+    childNodeHtml += "</select>";
+    
+    childNodeHtml += "</hr>";
+
+    node.innerHTML += childNodeHtml;
+        }
+    }
     function handleChange(_event: Event): void {
 
     }
