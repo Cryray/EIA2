c2555f6dfcefde3cc726b947351c5ce0d88b2850
Cryray <43845424+Cryray@users.noreply.github.com>
2018-12-02 14:03:14 +0100

    Neue Seite

--- Weihnachtsbaum NEW/newchristmas.ts
+++ Weihnachtsbaum NEW/newchristmas.ts
@@ -1,117 +1,108 @@
-namespace Wbk {
-        
-        interface HeteroPredef {
-            name: string;
-            price: number;
-            
-        }
-
-     
-        interface HomoVar {
-            [key: string]: HeteroPredef[];
-        }
-
-        
-            export let _data: HomoVar = {
-                "tree": [
-                    { name: "fir", price: 15 },
-                    { name: "Caucasian fir", price: 17 },
-                    { name: "Norway spruce", price: 18 },
-                    { name: "Bluespruce", price: 16 },
-                    { name: "Pine", price: 16 }
-                ],
-                "ball": [
-                    { name: "red", price: 1 },
-                    { name: "green", price: 1.50 },
-                    { name: "blue", price: 2 },
-                    { name: "gold", price: 1.50 },
-                    { name: "silver", price: 1 }
-                ],
-                 "tinsel": [
-                    { name: "red", price: 3 },
-                    { name: "green", price: 3.50 },
-                    { name: "blue", price: 3 },
-                    { name: "gold", price: 3.50 },
-                    { name: "silver", price: 3 }
-                ],
-                  "candle": [
-                    { name: "red", price: 2 },
-                    { name: "white", price: 2.50 },
-                    { name: "honey", price: 2 }
-                   
-                ],
-                   "shipment": [
-                    { name: "DHL", price: 5.99 },
-                    { name: "Hermes", price: 6.50 },
-                    { name: "UPS", price: 6 }
-                   
-                ]
-            };
-    
-
-   
-        document.addEventListener("DOMContentLoaded", init);
-
+namespace HolyChristmasTree2 {
+    window.addEventListener("DOMContentLoaded", init);
 
     function init(): void {
-        createFormat(_data);
-        }
+        createInput();
+      //  document.getElementById("button").addEventListener("click", checkOrder);
+    }
 
+    function createInput(): void {
 
+        for (let key in offers) {
+            let product: Product[] = offers[key];
 
+            let fieldset: HTMLElement = document.getElementById(key);
+            fieldset.addEventListener("change", createCart);
 
+            for (let a: number = 0; a < product.length; a++) {
 
-       /* function init(): void {
-            let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-            for (let i: number = 0; i < 1; i++) {
-                let fieldset: HTMLFieldSetElement = fieldsets[i];
-                createFormat(fieldset, data);
-                // fieldset.addEventListener("change", handleChange);
+                let input: HTMLInputElement = document.createElement("input");
+                document.getElementById(key).appendChild(input);
+                input.name = key;
+
+                input.value = offers[key][a].name;
+                input.id = offers[key][a].name;
+                input.setAttribute("group", key);
+                input.setAttribute("index", "" + a);
+                input.setAttribute("price", "" + offers[key][a].price);
+
+                let label: HTMLLabelElement = document.createElement("label");
+                document.getElementById(key).appendChild(label);
+                label.setAttribute("for", offers[key][a].name);
+                label.innerHTML = offers[key][a].name + " " + offers[key][a].price + "Euro";
+
+                if (key == "trees" || key == "treeholder" || key == "top" || key == "lightstrings") {
+                    input.type = "radio";
+                } else {
+
+                    input.type = "number";
+                    input.name = offers[key][a].name;
+                    input.step = "1.0";
+                    input.min = "0";
+                    input.max = "100";
+                    input.value = "0";
+
+                }
+
+                document.getElementById(key).appendChild(document.createElement("br"));
             }
-        }*/
-        function createFormat( _homoVar: HomoVar): void {
-            for (let key in _homoVar) {
-                let value: HeteroPredef[] = _homoVar[key];
-                let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-               for (let b: number= 0; b < 5; b++ ) {
-                let i: number=0 + b; i < value.length;
-                let fieldset: HTMLFieldSetElement = fieldsets[i];
-                    for (let a: number = 0; a < value.length; a++) {
-                        let input: HTMLInputElement = document.createElement("input");
-                        fieldset.appendChild(input);
-                        input.type = "checkbox";
-                        input.name = "type";
-                        input.value = value[a].name;
-                        input.id = value[a].name;
-                        let label: HTMLLabelElement = document.createElement("label");
-                        fieldset.appendChild(label);
-                        chooseNumber(a, fieldset, value);
-                        label.setAttribute("chosenType", value[a].name);
-                        label.innerHTML = value[a].name + " " + value[a].price + "Euro ";
-                    }
+        }
+    }
+
+
+    function createCart(): void {
+
+        let p: HTMLElement = document.getElementById("cart");
+        let total: number = 0;
+        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+        p.innerHTML = " ";
+        p.innerHTML = "<h2>Warenkorb</h2>";
+        for (let i: number = 0; i < inputs.length; i++) {
+
+            let input: HTMLInputElement = inputs[i];
+            if (input.type == "number") {
+                if (parseInt(input.value) > 0) {
+                    let value: number = parseFloat(input.value);
+                    let name: string = input.getAttribute("name");
+                    let price: number = parseFloat(input.getAttribute("price"));
+
+                    p.innerHTML += "<p>" + value + "x " + name + " " + " " + (price * value).toFixed(2) + "Euro" + " " + "</p>";
+                    total += parseFloat((price * value).toFixed(2));
                 }
             }
+            if (input.checked == true) {
+                let value: string = input.getAttribute("value");
+                let price: number = parseFloat(input.getAttribute("price"));
+                total += parseFloat((price).toFixed(2));
+
+                p.innerHTML += "<p>" + value + " " + " " + price + "Euro" + "</p>";
+
+            }
+
+        }
+        p.innerHTML += "Geamtpreis " + total.toFixed(2) + "Euro";
     }
-            function chooseNumber(_a: number, _fieldset: HTMLFieldSetElement, _value: HeteroPredef[]): void {
-                let input: HTMLInputElement = document.createElement("input");
-                _fieldset.appendChild(input);
-                input.type = "number";
-                input.min = "0";
-                input.max = "50";
-                input.step = "1";
-                input.value = _value[_a].name;
-                input.id = _value[_a].name;
-                let label: HTMLLabelElement = document.createElement("label");
-                _fieldset.appendChild(label);
-            }
 
-           /*  function handleChange(_event: Event): void {
-                 let target: HTMLInputElement = <HTMLInputElement>_event.target;
-                 if (this.id == value[a].name)
-                 console.log("Changed " + target.name + " to " + target.checked);}*/
-                 
-                   
-             
-            
-
+/*    function checkOrder(_event: Event): void {
+        for (let key in offers) {
+            let category: Product[] = offers[key];
+            let fieldset: HTMLElement = document.getElementById(key);
+            let elements: HTMLCollection = <HTMLInputElement>fieldset.getElementsByClassName
+            for (let i: number = 0; 0 < i++) {
+                if ()
             }
+            //            document.getElementById("Lieferoptionen");
+            //            document.getElementById("text");
+            //            for (let a: number = 0; a < product.length; a++)
+            //                if (key == "tree" || "treeholder" || "Lieferoptionen" || "text") {
+            //                    document.getElementById("notSelectedYet").innerHTML = "I'm sorry, you need to fill out a few more things!"; 
+            //                }
+            //                   else {
+            //                    document.getElementById(el).innerHTML = "All done, thank you for your order!";
+            //                    } 
+        }
+    }*/
+
+
+
+}
