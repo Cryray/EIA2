b85a3c7835506b9ea693489fc87f472f76846b8e
Cryray <43845424+Cryray@users.noreply.github.com>
2018-12-06 10:22:38 +0100

    liol

--- Weihnachtsbaum NEW/newchristmas.js
+++ Weihnachtsbaum NEW/newchristmas.js
@@ -1,85 +1,68 @@
-var Wbk;
-(function (Wbk) {
-    Wbk._data = {
-        "tree": [
-            { name: "fir", price: 15 },
-            { name: "Caucasian fir", price: 17 },
-            { name: "Norway spruce", price: 18 },
-            { name: "Bluespruce", price: 16 },
-            { name: "Pine", price: 16 }
-        ],
-        "ball": [
-            { name: "red", price: 1 },
-            { name: "green", price: 1.50 },
-            { name: "blue", price: 2 },
-            { name: "gold", price: 1.50 },
-            { name: "silver", price: 1 }
-        ],
-        "tinsel": [
-            { name: "red", price: 3 },
-            { name: "green", price: 3.50 },
-            { name: "blue", price: 3 },
-            { name: "gold", price: 3.50 },
-            { name: "silver", price: 3 }
-        ],
-        "candle": [
-            { name: "red", price: 2 },
-            { name: "white", price: 2.50 },
-            { name: "honey", price: 2 }
-        ],
-        "shipment": [
-            { name: "DHL", price: 5.99 },
-            { name: "Hermes", price: 6.50 },
-            { name: "UPS", price: 6 }
-        ]
-    };
-    document.addEventListener("DOMContentLoaded", init);
+var HolyChristmasTree2;
+(function (HolyChristmasTree2) {
+    window.addEventListener("DOMContentLoaded", init);
     function init() {
-        createFormat(Wbk._data);
+        createInput();
+        //  document.getElementById("button").addEventListener("click", checkOrder);
     }
-    /* function init(): void {
-         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-         for (let i: number = 0; i < 1; i++) {
-             let fieldset: HTMLFieldSetElement = fieldsets[i];
-             createFormat(fieldset, data);
-             // fieldset.addEventListener("change", handleChange);
-         }
-     }*/
-    function createFormat(_homoVar) {
-        for (let key in _homoVar) {
-            let value = _homoVar[key];
-            let fieldsets = document.getElementsByTagName("fieldset");
-            for (let b = 0; b < 5; b++) {
-                let i = 0 + b;
-                i < value.length;
-                let fieldset = fieldsets[i];
-                for (let a = 0; a < value.length; a++) {
-                    let input = document.createElement("input");
-                    fieldset.appendChild(input);
-                    input.type = "checkbox";
-                    input.name = "type";
-                    input.value = value[a].name;
-                    input.id = value[a].name;
-                    let label = document.createElement("label");
-                    fieldset.appendChild(label);
-                    chooseNumber(a, fieldset, value);
-                    label.setAttribute("chosenType", value[a].name);
-                    label.innerHTML = value[a].name + " " + value[a].price + "Euro ";
+    function createInput() {
+        for (let key in HolyChristmasTree2.offers) {
+            let product = HolyChristmasTree2.offers[key];
+            let fieldset = document.getElementById(key);
+            fieldset.addEventListener("change", createCart);
+            for (let a = 0; a < product.length; a++) {
+                let input = document.createElement("input");
+                document.getElementById(key).appendChild(input);
+                input.name = key;
+                input.value = HolyChristmasTree2.offers[key][a].name;
+                input.id = HolyChristmasTree2.offers[key][a].name;
+                input.setAttribute("group", key);
+                input.setAttribute("index", "" + a);
+                input.setAttribute("price", "" + HolyChristmasTree2.offers[key][a].price);
+                let label = document.createElement("label");
+                document.getElementById(key).appendChild(label);
+                label.setAttribute("for", HolyChristmasTree2.offers[key][a].name);
+                label.innerHTML = HolyChristmasTree2.offers[key][a].name + " " + HolyChristmasTree2.offers[key][a].price + "Euro";
+                if (key == "trees" || key == "treeholder" || key == "top" || key == "lightstrings") {
+                    input.type = "radio";
                 }
+                else {
+                    input.type = "number";
+                    input.name = HolyChristmasTree2.offers[key][a].name;
+                    input.step = "1.0";
+                    input.min = "0";
+                    input.max = "100";
+                    input.value = "0";
+                }
+                document.getElementById(key).appendChild(document.createElement("br"));
             }
         }
     }
-    function chooseNumber(_a, _fieldset, _value) {
-        let input = document.createElement("input");
-        _fieldset.appendChild(input);
-        input.type = "number";
-        input.min = "0";
-        input.max = "50";
-        input.step = "1";
-        input.value = _value[_a].name;
-        input.id = _value[_a].name;
-        let label = document.createElement("label");
-        _fieldset.appendChild(label);
+    function createCart() {
+        let p = document.getElementById("cart");
+        let total = 0;
+        let inputs = document.getElementsByTagName("input");
+        p.innerHTML = " ";
+        p.innerHTML = "<h2>Warenkorb</h2>";
+        for (let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+            if (input.type == "number") {
+                if (parseInt(input.value) > 0) {
+                    let value = parseFloat(input.value);
+                    let name = input.getAttribute("name");
+                    let price = parseFloat(input.getAttribute("price"));
+                    p.innerHTML += "<p>" + value + "x " + name + " " + " " + (price * value).toFixed(2) + "Euro" + " " + "</p>";
+                    total += parseFloat((price * value).toFixed(2));
+                }
+            }
+            if (input.checked == true) {
+                let value = input.getAttribute("value");
+                let price = parseFloat(input.getAttribute("price"));
+                total += parseFloat((price).toFixed(2));
+                p.innerHTML += "<p>" + value + " " + " " + price + "Euro" + "</p>";
+            }
+        }
+        p.innerHTML += "Geamtpreis " + total.toFixed(2) + "Euro";
     }
-})(Wbk || (Wbk = {}));
+})(HolyChristmasTree2 || (HolyChristmasTree2 = {}));
 //# sourceMappingURL=newchristmas.js.map
