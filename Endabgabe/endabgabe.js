65d298162b8b6754d145cb5deff044fc3e84048e
Cryray <43845424+Cryray@users.noreply.github.com>
2019-01-24 12:31:56 +0100

    ya yeet

---
+++ Endabgabe/endabgabe.js
@@ -0,0 +1,35 @@
+let crc2;
+window.addEventListener("load", init);
+function init(_event) {
+    let canvas = document.getElementsByTagName("canvas")[0];
+    crc2 = canvas.getContext("2d");
+    drawHill();
+    drawSky();
+}
+function drawHill() {
+    console.log("curve");
+    crc2.fillStyle = "#ffffff";
+    crc2.strokeStyle = "#000000";
+    crc2.beginPath();
+    crc2.moveTo(0, 300);
+    crc2.bezierCurveTo(110, 500, 220, 410, 360, 500);
+    crc2.lineTo(700, 700);
+    crc2.lineTo(0, 700);
+    crc2.closePath();
+    crc2.lineWidth = 2;
+    crc2.stroke();
+    crc2.fill();
+}
+function drawSky() {
+    crc2.fillStyle = "#A9E2F3";
+    crc2.beginPath();
+    crc2.moveTo(0, 300);
+    crc2.bezierCurveTo(110, 500, 220, 410, 360, 500);
+    crc2.lineTo(700, 0);
+    crc2.lineTo(0, 0);
+    crc2.closePath();
+    crc2.lineWidth = 2;
+    crc2.stroke();
+    crc2.fill();
+}
+//# sourceMappingURL=endabgabe.js.map
