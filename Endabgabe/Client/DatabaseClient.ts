34e920d737787a33fcddbfc4dfa4cda565475840
Cryray <43845424+Cryray@users.noreply.github.com>
2019-02-17 12:32:13 +0100

    try try try try fick mich

--- Endabgabe/Client/DatabaseClient.ts
+++ Endabgabe/Client/DatabaseClient.ts
@@ -1,3 +1,4 @@
+/*
 import * as Mongo from "mongodb";
 
 namespace DatabaseClient {
@@ -80,3 +81,82 @@ namespace DatabaseClient {
         }
     }
 }
+*/
+
+
+ window.addEventListener("load", init);
+
+    let serverAddress: string = "https://githubcryray.herokuapp.com/";
+
+    function init(_event: Event): void {
+        console.log("Init");
+        let insertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("sendButton");
+        let refreshButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("highscoreButton");
+
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+
+    }
+
+    function insert(_event: Event): void {
+        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+        let query: string = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&score=" + document.getElementById("finalScore").getAttribute("value");
+
+
+        sendRequest(query, handleInsertResponse);
+
+    }
+
+    function refresh(_event: Event): void {
+        let query: string = "command=refresh";
+        sendRequest(query, handleFindResponse);
+    }
+
+
+
+
+
+
+    function sendRequest(_query: string, _callback: EventListener): void {
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open("GET", serverAddress + "?" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+
+    function handleInsertResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+
+ 
+    function playerDataSort(_a: playerData, _b: playerData): number {
+        let returnNumber: number;
+        if (_a.score > _b.score) {
+            returnNumber = -1;
+        }
+        else if (_a.score < _b.score) {
+            returnNumber = 1;
+        }
+        else {
+            returnNumber = 0;
+        }
+        return returnNumber;
+
+    }
+
+    function handleFindResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let output: HTMLElement = document.getElementById("highscores");
+            let dataArray: playerData[] = JSON.parse(xhr.response);
+            dataArray.sort(playerDataSort);
+            for (let i: number = 0; i < dataArray.length; i++) {
+                output.innerHTML += "<p id='showScores'><strong>Name: </strong>" + dataArray[i].name + "<br><strong>Score: </strong>" + dataArray[i].score + "</p>";
+            }
+        }
+    }
--- Endabgabe/Server/Database.js
+++ Endabgabe/Server/Database.js
@@ -1,19 +1,93 @@
-"use strict";
 /**
  * Simple database insertion and query for MongoDB
  * @author: Jirka Dell'Oro-Friedl
  */
-const Mongo = require("mongodb");
+/*
+import * as Mongo from "mongodb";
 console.log("Database starting");
-let databaseURL = "mongodb://localhost:27017";
-let databaseName = "Test";
-let db;
-let hs;
+
+let databaseURL: string = "mongodb://localhost:27017";
+let databaseName: string = "Test";
+let db: Mongo.Db;
+let hs: Mongo.Collection;
+
 // running on heroku?
 if (process.env.NODE_ENV == "production") {
     //    databaseURL = "mongodb://username:password@hostname:port/database";
     databaseURL = "mongodb://testuser:testpw1@ds331735.mlab.com:31735/rht";
     databaseName = "rht";
+}
+
+// try to connect to database, then activate callback "handleConnect"
+Mongo.MongoClient.connect(databaseURL, handleConnect);
+
+// connect-handler receives two standard parameters, an error object and a database object
+function handleConnect(_e: Mongo.MongoError, _db: Mongo.Db): void {
+    if (_e)
+        console.log("Unable to connect to database, error: ", _e);
+    else {
+        console.log("Connected to database!");
+        db = _db.db(databaseName);
+        hs = db.collection("Highscores");
+    }
+}
+
+export function insert(_doc: playerData): void {
+    // try insertion then activate callback "handleInsert"
+    hs.insertOne(_doc, handleInsert);
+}
+
+// insertion-handler receives an error object as standard parameter
+function handleInsert(_e: Mongo.MongoError): void {
+    console.log("Database insertion returned -> " + _e);
+}
+export function search(_callback: Function, _matrikel: string): void {
+    var cursor: Mongo.Cursor = hs.find();
+    cursor.toArray(prepareAnswer);
+    function prepareAnswer(_e: Mongo.MongoError, playerArray: playerData[]): void {
+        if (_e)
+            _callback("Error" + _e);
+        else
+
+            for (let i: number = 0; i < playerArray.length; i++) {
+                if (playerArray[i].score == Number(_matrikel)) {
+                    _callback(JSON.stringify(playerArray[i]));
+                }
+            }
+    }
+}
+
+
+// try to fetch all documents from database, then activate callback
+export function findAll(_callback: Function): void {
+    // cursor points to the retreived set of documents in memory
+    var cursor: Mongo.Cursor = hs.find();
+    // try to convert to array, then activate callback "prepareAnswer"
+    cursor.toArray(prepareAnswer);
+
+    // toArray-handler receives two standard parameters, an error object and the array
+    // implemented as inner function, so _callback is in scope
+    function prepareAnswer(_e: Mongo.MongoError, playerArray: playerData[]): void {
+        if (_e)
+            _callback("Error" + _e);
+        else
+            // stringify creates a json-string, passed it back to _callback
+            _callback(JSON.stringify(playerArray));
+    }
+}
+*/
+"use strict";
+const Mongo = require("mongodb");
+console.log("Database starting");
+let databaseURL = "mongodb://localhost:27017";
+let databaseName = "test";
+let db;
+let students;
+// running on heroku?
+if (process.env.NODE_ENV == "production") {
+    //    databaseURL = "mongodb://username:password@hostname:port/database";
+    databaseURL = "mongodb://test:test1234@ds159112.mlab.com:59112/eia2";
+    databaseName = "eia2";
 }
 // try to connect to database, then activate callback "handleConnect" 
 Mongo.MongoClient.connect(databaseURL, handleConnect);
@@ -24,12 +98,12 @@ function handleConnect(_e, _db) {
     else {
         console.log("Connected to database!");
         db = _db.db(databaseName);
-        hs = db.collection("Highscores");
+        students = db.collection("scores");
     }
 }
 function insert(_doc) {
     // try insertion then activate callback "handleInsert"
-    hs.insertOne(_doc, handleInsert);
+    students.insertOne(_doc, handleInsert);
 }
 exports.insert = insert;
 // insertion-handler receives an error object as standard parameter
@@ -37,34 +111,28 @@ function handleInsert(_e) {
     console.log("Database insertion returned -> " + _e);
 }
 function search(_callback, _matrikel) {
-    var cursor = hs.find();
+    var cursor = students.find();
     cursor.toArray(prepareAnswer);
-    function prepareAnswer(_e, playerArray) {
+    function prepareAnswer(_e, studentArray) {
         if (_e)
             _callback("Error" + _e);
-        else
-            for (let i = 0; i < playerArray.length; i++) {
-                if (playerArray[i].score == Number(_matrikel)) {
-                    _callback(JSON.stringify(playerArray[i]));
-                }
-            }
     }
 }
 exports.search = search;
 // try to fetch all documents from database, then activate callback
 function findAll(_callback) {
     // cursor points to the retreived set of documents in memory
-    var cursor = hs.find();
+    var cursor = students.find();
     // try to convert to array, then activate callback "prepareAnswer"
     cursor.toArray(prepareAnswer);
     // toArray-handler receives two standard parameters, an error object and the array
     // implemented as inner function, so _callback is in scope
-    function prepareAnswer(_e, playerArray) {
+    function prepareAnswer(_e, studentArray) {
         if (_e)
             _callback("Error" + _e);
         else
             // stringify creates a json-string, passed it back to _callback
-            _callback(JSON.stringify(playerArray));
+            _callback(JSON.stringify(studentArray));
     }
 }
 exports.findAll = findAll;
